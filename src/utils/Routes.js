aee95a7623844ad833b950fac8574b0ab241fb58
Rajnesh Prajapati <raznesh.prajapati@gmail.com>
2021-06-30 10:33:39 +0545

    Menu page has been add

--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -1,16 +1,20 @@
 import { Switch, Route } from "react-router-dom";
 
 import DigitalAgencyLandingPage from "../Digital Agency Landing Page/DigitalAgencyLandingPage";
+import MainMenu from "../MainMenu";
 import TravelWebsiteHomePage from "../Travel Website Home Page/TravelWebsiteHomePage";
 
 const Routes = () => {
   return (
     <>
       <Switch>
+        <Route exact path="/">
+          <MainMenu />
+        </Route>
         <Route exact path="/digitalAgencyLandingPage">
           <DigitalAgencyLandingPage />
         </Route>
-        <Route exact path="/">
+        <Route exact path="/travelWebsiteHomePage">
           <TravelWebsiteHomePage />
         </Route>
       </Switch>
